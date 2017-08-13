76b89a3e9168a5e478af3bc9ab45073bd6fda72b
Matt Shen <lovelywib@gmail.com>
2017-08-13 15:59:49 +1000

    bulk insert

--- routes/insert.js
+++ routes/insert.js
@@ -6,9 +6,12 @@ const router = express.Router();
 
 router.post('/', function (req, res, next) {
   const table = req.body.table;
-  const record = req.body.record;
+  const records = req.body.records;
+  const columns = Object.keys(records[0])
+  const values = records.map(record => columns.map( column => record[column]));
 
-  connection.query(`INSERT INTO ${table} set ?`, record, function (err, result) {
+  const joinedColumns = columns.join(', ');
+  connection.query(`INSERT INTO ${table} (${joinedColumns}) values ?`, [values], function (err, result) {
     if (!err) {
       let response = null;
       if (result.affectedRows != 0) {
